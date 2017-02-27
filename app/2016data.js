04a308ef7e10d08870d98305cfc4570dc042f395
Logan Garland <lgarland@marana.com>
2017-02-27 10:17:06 -0700

    - week 6 update

---
+++ app/2016data.js
@@ -0,0 +1,325 @@
+var data = [
+  {
+    name: 'Logan',
+    start: 237.4,
+    weeks: [
+      {week: 0, weight: 237.4},
+      {week: 1, weight: 233.4},
+      {week: 2, weight: 233.4},
+      {week: 3, weight: 233.4},
+      {week: 4, weight: 233.4},
+      {week: 5, weight: 233.4},
+      {week: 6, weight: 233.4},
+      {week: 7, weight: 233.4},
+      {week: 8, weight: 233.4},
+      {week: 9, weight: 233.4},
+      {week: 10, weight: 233.4},
+      {week: 11, weight: 233.4},
+      {week: 12, weight: 233.4}
+
+    ],
+    show: true
+  },
+  {
+    name: 'Hilary',
+    start: 182.2,
+    weeks: [
+      {week: 0, weight: 182.2},
+      {week: 1, weight: 181.0},
+      {week: 2, weight: 177.0},
+      {week: 3, weight: 179.0},
+      {week: 4, weight: 177.8},
+      {week: 5, weight: 177.8},
+      {week: 6, weight: 177.8},
+      {week: 7, weight: 177.8},
+      {week: 8, weight: 177.8},
+      {week: 9, weight: 177.8},
+      {week: 10, weight: 177.8},
+      {week: 11, weight: 177.8},
+      {week: 12, weight: 176.6}
+
+    ],
+    show: false
+  },
+  {
+    name: 'Chrissy',
+    start: 143.2,
+    weeks: [
+      {week: 0, weight: 143.2},
+      {week: 1, weight: 142.4},
+      {week: 2, weight: 139.6},
+      {week: 3, weight: 140.2},
+      {week: 4, weight: 140.8},
+      {week: 5, weight: 140.2},
+      {week: 6, weight: 139.0},
+      {week: 7, weight: 138.0},
+      {week: 8, weight: 138.0},
+      {week: 9, weight: 138.0},
+      {week: 10, weight: 138.0},
+      {week: 11, weight: 138.0},
+      {week: 12, weight: 134.2}
+
+
+    ],
+    show: true
+  },
+  {
+    name: 'Suzanne',
+    start: 150.6,
+    weeks: [
+      {week: 0, weight: 150.6},
+      {week: 1, weight: 149.8},
+      {week: 2, weight: 151.2},
+      {week: 3, weight: 151.2},
+      {week: 4, weight: 151.2},
+      {week: 5, weight: 151.2},
+      {week: 7, weight: 151.2},
+      {week: 8, weight: 151.2},
+      {week: 9, weight: 151.2},
+      {week: 10, weight: 151.2},
+      {week: 11, weight: 151.2},
+      {week: 12, weight: 151.2}
+    ],
+    show: false
+  },
+  {
+    name: 'Nolette',
+    start: 231.0,
+    weeks: [
+      {week: 0, weight: 231.0},
+      {week: 1, weight: 231.0},
+      {week: 2, weight: 232.0},
+      {week: 3, weight: 226.4},
+      {week: 4, weight: 230.4},
+      {week: 5, weight: 230.4},
+      {week: 6, weight: 227.6},
+      {week: 7, weight: 227.6},
+      {week: 8, weight: 227.6},
+      {week: 9, weight: 227.6},
+      {week: 10, weight: 227.6},
+      {week: 11, weight: 227.6},
+      {week: 12, weight: 227.6}
+    ],
+    show: false
+  },
+  {
+    name: 'Brian',
+    start: 233.6,
+    weeks: [
+      {week: 0, weight: 233.6},
+      {week: 1, weight: 233.6},
+      {week: 2, weight: 233.6},
+      {week: 3, weight: 233.6},
+      {week: 4, weight: 233.6}
+    ],
+    show: true
+  },
+  {
+    name: 'Leanne',
+    start: 164.8,
+    weeks: [
+      {week: 0, weight: 164.8},
+      {week: 1, weight: 164.8},
+      {week: 2, weight: 164.8},
+      {week: 3, weight: 164.8},
+      {week: 4, weight: 164.8}
+    ],
+    show: true
+  },
+  {
+    name: 'Matt',
+    start: 263.0,
+    weeks: [
+      {week: 0, weight: 263.0},
+      {week: 1, weight: 261.0},
+      {week: 2, weight: 264.3},
+      {week: 3, weight: 262.8},
+      {week: 4, weight: 261.8},
+      {week: 5, weight: 262.6},
+      {week: 6, weight: 262.6},
+      {week: 7, weight: 262.6},
+      {week: 8, weight: 262.6}
+
+    ],
+    show: true
+  },
+  {
+    name: 'Mitch',
+    start: 239.2,
+    weeks: [
+      {week: 0, weight: 239.2},
+      {week: 1, weight: 237.8},
+      {week: 2, weight: 237.2},
+      {week: 3, weight: 236.8},
+      {week: 4, weight: 235.6},
+      {week: 5, weight: 233.4},
+      {week: 6, weight: 232.8},
+      {week: 7, weight: 232.8},
+      {week: 8, weight: 232.2},
+      {week: 9, weight: 232.8},
+      {week: 10, weight: 232.6},
+      {week: 11, weight: 233.4},
+      {week: 12, weight: 234.8}
+    ],
+    show: true
+  },
+  {
+    name: 'Emily',
+    start: 173.4,
+    weeks: [
+      {week: 0, weight: 173.4},
+      {week: 1, weight: 173.4},
+      {week: 2, weight: 173.4},
+      {week: 3, weight: 173.4},
+      {week: 4, weight: 173.4}
+    ],
+    show: false
+  },
+  {
+    name: 'Mike',
+    start: 204.4,
+    weeks: [
+      {week: 0, weight: 204.4},
+      {week: 1, weight: 201.4},
+      {week: 2, weight: 201.4},
+      {week: 3, weight: 201.4},
+      {week: 4, weight: 201.4}
+    ],
+    show: true
+  },
+  {
+    name: 'Jocelyn',
+    start: 151.0,
+    weeks: [
+      {week: 0, weight: 151.0},
+      {week: 1, weight: 151.2},
+      {week: 2, weight: 149.0},
+      {week: 3, weight: 148.4},
+      {week: 4, weight: 148.4},
+      {week: 5, weight: 148.4},
+      {week: 6, weight: 148.4},
+      {week: 7, weight: 148.4},
+      {week: 8, weight: 148.4},
+      {week: 9, weight: 148.4},
+      {week: 10, weight: 148.4},
+      {week: 11, weight: 148.4},
+      {week: 12, weight: 148.8}
+    ],
+    show: false
+  },
+  {
+    name: 'Tracy',
+    start: 178.8,
+    weeks: [
+      {week: 0, weight: 178.8},
+      {week: 1, weight: 177.0},
+      {week: 2, weight: 176.6},
+      {week: 3, weight: 176.6},
+      {week: 4, weight: 176.6}
+    ],
+    show: false
+  },
+  {
+    name: 'Dawn',
+    start: 128.0,
+    weeks: [
+      {week: 0, weight: 128.0},
+      {week: 1, weight: 128.0},
+      {week: 2, weight: 128.0},
+      {week: 3, weight: 128.0},
+      {week: 4, weight: 125.4},
+      {week: 5, weight: 125.4},
+      {week: 6, weight: 125.4},
+      {week: 7, weight: 125.4},
+      {week: 8, weight: 125.4},
+      {week: 9, weight: 125.4},
+      {week: 10, weight: 125.4},
+      {week: 11, weight: 125.4},
+      {week: 12, weight: 123.4}
+
+    ],
+    show: true
+  },
+  {
+    name: 'Dan',
+    start: 260.0,
+    weeks: [
+      {week: 0, weight: 260.0},
+      {week: 1, weight: 259.0},
+      {week: 2, weight: 259.0},
+      {week: 3, weight: 259.0},
+      {week: 4, weight: 259.0}
+    ],
+    show: true
+  },
+  {
+    name: 'Mayra',
+    start: 131.4,
+    weeks: [
+      {week: 0, weight: 131.4},
+      {week: 1, weight: 131.4},
+      {week: 2, weight: 129.8},
+      {week: 3, weight: 129.8},
+      {week: 4, weight: 129.8},
+      {week: 5, weight: 129.8},
+      {week: 6, weight: 133.2}
+
+    ],
+    show: false
+  },
+  {
+    name: 'Sherri',
+    start: 233.0,
+    weeks: [
+      {week: 0, weight: 233.0},
+      {week: 1, weight: 233.0},
+      {week: 2, weight: 233.0},
+      {week: 3, weight: 233.0},
+      {week: 4, weight: 233.0}
+    ],
+    show: false
+  },
+  {
+    name: 'Libby',
+    start: 150.4,
+    weeks: [
+      {week: 0, weight: 150.4},
+      {week: 1, weight: 150.4},
+      {week: 2, weight: 149.0},
+      {week: 3, weight: 149.0},
+      {week: 4, weight: 149.0}
+    ],
+    show: false
+  },
+  {
+    name: 'Christen',
+    start: 145.2,
+    weeks: [
+      {week: 0, weight: 145.2},
+      {week: 1, weight: 145.2},
+      {week: 2, weight: 145.2},
+      {week: 3, weight: 144.4},
+      {week: 4, weight: 144.4}
+    ],
+    show: true
+  },
+  {
+    name: 'Linda',
+    start: 150.4,
+    weeks: [
+      {week: 0, weight: 150.4},
+      {week: 1, weight: 150.4},
+      {week: 2, weight: 150.4},
+      {week: 3, weight: 149.0},
+      {week: 4, weight: 149.0},
+      {week: 5, weight: 149.0},
+      {week: 6, weight: 149.0},
+      {week: 7, weight: 149.0},
+      {week: 8, weight: 149.0},
+      {week: 10, weight: 149.0},
+      {week: 11, weight: 149.0},
+      {week: 12, weight: 151.0}
+    ],
+    show: false
+  }
+];
--- app/data.js
+++ app/data.js
@@ -1,63 +1,45 @@
 var data = [
   {
     name: 'Logan',
-    start: 237.4,
+    start: 254.4,
     weeks: [
-      {week: 0, weight: 237.4},
-      {week: 1, weight: 233.4},
-      {week: 2, weight: 233.4},
-      {week: 3, weight: 233.4},
-      {week: 4, weight: 233.4},
-      {week: 5, weight: 233.4},
-      {week: 6, weight: 233.4},
-      {week: 7, weight: 233.4},
-      {week: 8, weight: 233.4},
-      {week: 9, weight: 233.4},
-      {week: 10, weight: 233.4},
-      {week: 11, weight: 233.4},
-      {week: 12, weight: 233.4}
+      {week: 0, weight: 254.4},
+      {week: 1, weight: 247.2},
+      {week: 2, weight: 247.2},
+      {week: 3, weight: 247.2},
+      {week: 4, weight: 247.2},
+      {week: 5, weight: 247.2},
+      {week: 6, weight: 247.2}
 
     ],
     show: true
   },
   {
     name: 'Hilary',
-    start: 182.2,
+    start: 176.4,
     weeks: [
-      {week: 0, weight: 182.2},
-      {week: 1, weight: 181.0},
-      {week: 2, weight: 177.0},
-      {week: 3, weight: 179.0},
-      {week: 4, weight: 177.8},
-      {week: 5, weight: 177.8},
-      {week: 6, weight: 177.8},
-      {week: 7, weight: 177.8},
-      {week: 8, weight: 177.8},
-      {week: 9, weight: 177.8},
-      {week: 10, weight: 177.8},
-      {week: 11, weight: 177.8},
-      {week: 12, weight: 176.6}
+      {week: 0, weight: 176.4},
+      {week: 1, weight: 176.6},
+      {week: 2, weight: 176.4},
+      {week: 3, weight: 176.4},
+      {week: 4, weight: 174.6},
+      {week: 5, weight: 174.6},
+      {week: 6, weight: 174.6}
 
     ],
     show: false
   },
   {
     name: 'Chrissy',
-    start: 143.2,
+    start: 140.8,
     weeks: [
-      {week: 0, weight: 143.2},
-      {week: 1, weight: 142.4},
-      {week: 2, weight: 139.6},
-      {week: 3, weight: 140.2},
-      {week: 4, weight: 140.8},
-      {week: 5, weight: 140.2},
-      {week: 6, weight: 139.0},
-      {week: 7, weight: 138.0},
-      {week: 8, weight: 138.0},
-      {week: 9, weight: 138.0},
-      {week: 10, weight: 138.0},
-      {week: 11, weight: 138.0},
-      {week: 12, weight: 134.2}
+      {week: 0, weight: 140.8},
+      {week: 1, weight: 139.8},
+      {week: 2, weight: 138.0},
+      {week: 3, weight: 138.8},
+      {week: 4, weight: 138.6},
+      {week: 5, weight: 140.0},
+      {week: 6, weight: 139.6}
 
 
     ],
@@ -65,260 +47,240 @@ var data = [
   },
   {
     name: 'Suzanne',
-    start: 150.6,
+    start: 160.6,
     weeks: [
-      {week: 0, weight: 150.6},
-      {week: 1, weight: 149.8},
-      {week: 2, weight: 151.2},
-      {week: 3, weight: 151.2},
-      {week: 4, weight: 151.2},
-      {week: 5, weight: 151.2},
-      {week: 7, weight: 151.2},
-      {week: 8, weight: 151.2},
-      {week: 9, weight: 151.2},
-      {week: 10, weight: 151.2},
-      {week: 11, weight: 151.2},
-      {week: 12, weight: 151.2}
+      {week: 0, weight: 160.6},
+      {week: 1, weight: 159.4},
+      {week: 2, weight: 159.8},
+      {week: 3, weight: 159.8},
+      {week: 4, weight: 159.8},
+      {week: 5, weight: 159.8},
+      {week: 6, weight: 159.8}
     ],
     show: false
   },
   {
     name: 'Nolette',
-    start: 231.0,
+    start: 230,
     weeks: [
-      {week: 0, weight: 231.0},
-      {week: 1, weight: 231.0},
-      {week: 2, weight: 232.0},
-      {week: 3, weight: 226.4},
-      {week: 4, weight: 230.4},
-      {week: 5, weight: 230.4},
-      {week: 6, weight: 227.6},
-      {week: 7, weight: 227.6},
-      {week: 8, weight: 227.6},
-      {week: 9, weight: 227.6},
-      {week: 10, weight: 227.6},
-      {week: 11, weight: 227.6},
-      {week: 12, weight: 227.6}
+      {week: 0, weight: 230},
+      {week: 1, weight: 228.6},
+      {week: 2, weight: 228.4},
+      {week: 3, weight: 228.8},
+      {week: 4, weight: 228.6},
+      {week: 5, weight: 225.6},
+      {week: 6, weight: 227.8}
     ],
     show: false
   },
   {
     name: 'Brian',
-    start: 233.6,
+    start: 242,
     weeks: [
-      {week: 0, weight: 233.6},
-      {week: 1, weight: 233.6},
-      {week: 2, weight: 233.6},
-      {week: 3, weight: 233.6},
-      {week: 4, weight: 233.6}
+      {week: 0, weight: 242},
+      {week: 1, weight: 242},
+      {week: 2, weight: 242},
+      {week: 4, weight: 242},
+      {week: 4, weight: 242},
+      {week: 5, weight: 242},
+      {week: 6, weight: 242}
     ],
     show: true
   },
   {
     name: 'Leanne',
-    start: 164.8,
+    start: 171.2,
     weeks: [
-      {week: 0, weight: 164.8},
-      {week: 1, weight: 164.8},
-      {week: 2, weight: 164.8},
-      {week: 3, weight: 164.8},
-      {week: 4, weight: 164.8}
-    ],
-    show: true
-  },
-  {
-    name: 'Matt',
-    start: 263.0,
-    weeks: [
-      {week: 0, weight: 263.0},
-      {week: 1, weight: 261.0},
-      {week: 2, weight: 264.3},
-      {week: 3, weight: 262.8},
-      {week: 4, weight: 261.8},
-      {week: 5, weight: 262.6},
-      {week: 6, weight: 262.6},
-      {week: 7, weight: 262.6},
-      {week: 8, weight: 262.6}
-
+      {week: 0, weight: 171.2},
+      {week: 1, weight: 169},
+      {week: 2, weight: 169},
+      {week: 3, weight: 169},
+      {week: 4, weight: 169},
+      {week: 5, weight: 169},
+      {week: 6, weight: 169}
     ],
     show: true
   },
   {
     name: 'Mitch',
-    start: 239.2,
+    start: 231.8,
     weeks: [
-      {week: 0, weight: 239.2},
-      {week: 1, weight: 237.8},
-      {week: 2, weight: 237.2},
-      {week: 3, weight: 236.8},
-      {week: 4, weight: 235.6},
-      {week: 5, weight: 233.4},
-      {week: 6, weight: 232.8},
-      {week: 7, weight: 232.8},
-      {week: 8, weight: 232.2},
-      {week: 9, weight: 232.8},
-      {week: 10, weight: 232.6},
-      {week: 11, weight: 233.4},
-      {week: 12, weight: 234.8}
+      {week: 0, weight: 231.8},
+      {week: 1, weight: 231.2},
+      {week: 2, weight: 233.8},
+      {week: 3, weight: 231.2},
+      {week: 4, weight: 231},
+      {week: 5, weight: 232},
+      {week: 6, weight: 231.6}
     ],
     show: true
   },
   {
     name: 'Emily',
-    start: 173.4,
+    start: 178,
     weeks: [
-      {week: 0, weight: 173.4},
-      {week: 1, weight: 173.4},
-      {week: 2, weight: 173.4},
-      {week: 3, weight: 173.4},
-      {week: 4, weight: 173.4}
+      {week: 0, weight: 178},
+      {week: 1, weight: 178.6},
+      {week: 2, weight: 178.6},
+      {week: 3, weight: 178.6},
+      {week: 4, weight: 178.6},
+      {week: 5, weight: 178.6},
+      {week: 6, weight: 178.6}
     ],
     show: false
   },
   {
     name: 'Mike',
-    start: 204.4,
+    start: 208.2,
     weeks: [
-      {week: 0, weight: 204.4},
-      {week: 1, weight: 201.4},
-      {week: 2, weight: 201.4},
-      {week: 3, weight: 201.4},
-      {week: 4, weight: 201.4}
+      {week: 0, weight: 208.2},
+      {week: 1, weight: 208.2},
+      {week: 2, weight: 208.2},
+      {week: 3, weight: 208.2},
+      {week: 4, weight: 208.2},
+      {week: 5, weight: 208.2},
+      {week: 6, weight: 208.2}
     ],
     show: true
   },
   {
-    name: 'Jocelyn',
-    start: 151.0,
-    weeks: [
-      {week: 0, weight: 151.0},
-      {week: 1, weight: 151.2},
-      {week: 2, weight: 149.0},
-      {week: 3, weight: 148.4},
-      {week: 4, weight: 148.4},
-      {week: 5, weight: 148.4},
-      {week: 6, weight: 148.4},
-      {week: 7, weight: 148.4},
-      {week: 8, weight: 148.4},
-      {week: 9, weight: 148.4},
-      {week: 10, weight: 148.4},
-      {week: 11, weight: 148.4},
-      {week: 12, weight: 148.8}
-    ],
-    show: false
-  },
-  {
     name: 'Tracy',
-    start: 178.8,
+    start: 183.6,
     weeks: [
-      {week: 0, weight: 178.8},
-      {week: 1, weight: 177.0},
-      {week: 2, weight: 176.6},
-      {week: 3, weight: 176.6},
-      {week: 4, weight: 176.6}
+      {week: 0, weight: 183.6},
+      {week: 1, weight: 183.6},
+      {week: 2, weight: 183.6},
+      {week: 3, weight: 183.6},
+      {week: 4, weight: 183.6},
+      {week: 5, weight: 183.6},
+      {week: 6, weight: 183.6}
     ],
     show: false
   },
   {
     name: 'Dawn',
-    start: 128.0,
+    start: 132.2,
     weeks: [
-      {week: 0, weight: 128.0},
-      {week: 1, weight: 128.0},
-      {week: 2, weight: 128.0},
-      {week: 3, weight: 128.0},
-      {week: 4, weight: 125.4},
-      {week: 5, weight: 125.4},
-      {week: 6, weight: 125.4},
-      {week: 7, weight: 125.4},
-      {week: 8, weight: 125.4},
-      {week: 9, weight: 125.4},
-      {week: 10, weight: 125.4},
-      {week: 11, weight: 125.4},
-      {week: 12, weight: 123.4}
+      {week: 0, weight: 132.2},
+      {week: 1, weight: 127.4},
+      {week: 2, weight: 125.4},
+      {week: 3, weight: 123.2},
+      {week: 4, weight: 118.4},
+      {week: 5, weight: 118.4},
+      {week: 6, weight: 118.4}
 
     ],
     show: true
   },
   {
     name: 'Dan',
-    start: 260.0,
+    start: 252,
     weeks: [
-      {week: 0, weight: 260.0},
-      {week: 1, weight: 259.0},
-      {week: 2, weight: 259.0},
-      {week: 3, weight: 259.0},
-      {week: 4, weight: 259.0}
+      {week: 0, weight: 252},
+      {week: 1, weight: 251.4},
+      {week: 2, weight: 249.2},
+      {week: 3, weight: 248.8},
+      {week: 4, weight: 246.8},
+      {week: 5, weight: 246.8},
+      {week: 6, weight: 242.2}
     ],
     show: true
   },
   {
-    name: 'Mayra',
-    start: 131.4,
+    name: 'Cathy',
+    start: 187.8,
     weeks: [
-      {week: 0, weight: 131.4},
-      {week: 1, weight: 131.4},
-      {week: 2, weight: 129.8},
-      {week: 3, weight: 129.8},
-      {week: 4, weight: 129.8},
-      {week: 5, weight: 129.8},
-      {week: 6, weight: 133.2}
-
-    ],
-    show: false
-  },
-  {
-    name: 'Sherri',
-    start: 233.0,
-    weeks: [
-      {week: 0, weight: 233.0},
-      {week: 1, weight: 233.0},
-      {week: 2, weight: 233.0},
-      {week: 3, weight: 233.0},
-      {week: 4, weight: 233.0}
+      {week: 0, weight: 187.8},
+      {week: 1, weight: 187.8},
+      {week: 2, weight: 184.4},
+      {week: 3, weight: 178},
+      {week: 4, weight: 178},
+      {week: 5, weight: 178},
+      {week: 6, weight: 178}
     ],
     show: false
   },
   {
     name: 'Libby',
-    start: 150.4,
+    start: 153,
     weeks: [
-      {week: 0, weight: 150.4},
+      {week: 0, weight: 153},
+      {week: 1, weight: 153},
+      {week: 2, weight: 153},
+      {week: 3, weight: 153},
+      {week: 4, weight: 153},
+      {week: 5, weight: 153},
+      {week: 6, weight: 153}
+    ],
+    show: false
+  },
+  {
+    name: 'Cindy',
+    start: 138,
+    weeks: [
+      {week: 0, weight: 138},
+      {week: 1, weight: 138},
+      {week: 2, weight: 136.8},
+      {week: 3, weight: 136.6},
+      {week: 4, weight: 136.6},
+      {week: 5, weight: 136.6},
+      {week: 6, weight: 136.6}
+    ],
+    show: false
+  },
+  {
+    name: 'Shirley',
+    start: 152,
+    weeks: [
+      {week: 0, weight: 152},
       {week: 1, weight: 150.4},
-      {week: 2, weight: 149.0},
-      {week: 3, weight: 149.0},
-      {week: 4, weight: 149.0}
+      {week: 2, weight: 147.8},
+      {week: 3, weight: 147},
+      {week: 4, weight: 146.2},
+      {week: 5, weight: 146.2},
+      {week: 6, weight: 146.0}
+    ],
+    show: false
+  },
+  {
+    name: 'Jennifer',
+    start: 206.8,
+    weeks: [
+      {week: 0, weight: 206.8},
+      {week: 1, weight: 195.8},
+      {week: 2, weight: 192.6},
+      {week: 3, weight: 189},
+      {week: 4, weight: 181.2},
+      {week: 5, weight: 181.2},
+      {week: 6, weight: 181.2}
+    ],
+    show: false
+  },
+  {
+    name: 'Fausto',
+    start: 221.8,
+    weeks: [
+      {week: 0, weight: 221.8},
+      {week: 1, weight: 221.8},
+      {week: 2, weight: 221.6},
+      {week: 3, weight: 221.6},
+      {week: 4, weight: 213.4},
+      {week: 5, weight: 213.4},
+      {week: 6, weight: 213.4}
     ],
     show: false
   },
   {
     name: 'Christen',
-    start: 145.2,
+    start: 148.8,
     weeks: [
-      {week: 0, weight: 145.2},
-      {week: 1, weight: 145.2},
-      {week: 2, weight: 145.2},
-      {week: 3, weight: 144.4},
-      {week: 4, weight: 144.4}
-    ],
-    show: true
-  },
-  {
-    name: 'Linda',
-    start: 150.4,
-    weeks: [
-      {week: 0, weight: 150.4},
-      {week: 1, weight: 150.4},
-      {week: 2, weight: 150.4},
-      {week: 3, weight: 149.0},
-      {week: 4, weight: 149.0},
-      {week: 5, weight: 149.0},
-      {week: 6, weight: 149.0},
-      {week: 7, weight: 149.0},
-      {week: 8, weight: 149.0},
-      {week: 10, weight: 149.0},
-      {week: 11, weight: 149.0},
-      {week: 12, weight: 151.0}
+      {week: 0, weight: 148.8},
+      {week: 1, weight: 148.8},
+      {week: 2, weight: 144.4},
+      {week: 3, weight: 144.6},
+      {week: 4, weight: 146.4},
+      {week: 5, weight: 146.2},
+      {week: 6, weight: 145.2}
     ],
     show: false
   }
